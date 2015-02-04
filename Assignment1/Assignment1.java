d3aa06544e2eb3bb933823c9c3981ef343aa48f6
Harm Manders <harmmanders@gmail.com>
2015-02-04 16:26:19 +0100

    initial file commit

---
+++ Assignment1/Assignment1.java
@@ -0,0 +1,21 @@
+/*
+ * File: AdvObject.java
+ * --------------------
+ * This file defines a class that models an object in the
+ * Adventure game.
+ */
+ 
+import java.io.*;
+import java.util.*;
+ 
+
+public class Assignment1 extends java.lang.Object
+{   
+     static void main(java.lang.String[] arg) {
+
+     }
+
+     
+
+
+}
---
+++ Assignment1/CollectionTimer.java
@@ -0,0 +1,4 @@
+public abstract class CollectionTimer extends java.lang.Object
+{
+
+}
