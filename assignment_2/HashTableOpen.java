7c637e6e00e3367b9f41f95923ddaa87cd3320fc
maartje <maartjeterhoeve@gmail.com>
2015-02-15 23:43:46 +0100

    single linked list with put method probably working quite well

--- assignment_2/HashTableOpen.java
+++ assignment_2/HashTableOpen.java
@@ -3,7 +3,34 @@ import java.io.*;
 
 public class HashTableOpen {
 
-	// private int hash_size;
+    private CollisionChaining[] table;
+    private Compressable function;
+
+    /* Constructor */
+    HashTableOpen(int hash_size, Compressable function) {
+        this.function = function;
+        table = new CollisionChaining[hash_size]; //if you don't want table load factor to be 0, this should be different
+    }
+    
+    /* Puts new node to existing or new single linked list (represented as collisionChaining object) */
+    public void put (String key, String value) {
+        int index = function.calcIndex(key);
+        CollisionChaining currentListNode = table[index];
+        if (table[index] == null) {
+            table[index] = new CollisionChaining(key, value);
+        }
+        
+        /* Loop over single linked list untill you've reached the last node */
+        while (currentListNode.getNext() != null) {
+            currentListNode = table[index].getNext();
+        }
+        
+        if (currentListNode.getNext() == null) {
+            currentListNode.setNext(new CollisionChaining(key, value));
+        }
+    }
+
+	/* // private int hash_size;
 	private Compressable function;
 	private String[][] table;
 
@@ -12,19 +39,21 @@ public class HashTableOpen {
 		this.function = function;
 		table = new String[hash_size][2];
 	}
-
+	
 	public void put(String key, String value) {
 		int index = function.calcIndex(key);
 		table[index][0] = key; //put key in first column array
 		table[index][1] = value; //put value in second column array (value will be the placeholder a)
 	}
+	
+	
 
 	public String get(String key) {
 		int index = function.calcIndex(key);
 		//System.out.println(table[index][1]);
 		//System.out.println("check");
 		return table[index][0];
-	}
+	} */
 
 	public int size() {
 		return table.length;
