d05a3ca5aacf1d32d7dce22e1d865b2ad8aeaf78
Harm Manders <harmmanders@gmail.com>
2015-02-20 18:07:16 +0100

    main() edit

--- assignment_3/LionsLambs.java
+++ assignment_3/LionsLambs.java
@@ -7,9 +7,16 @@ import java.awt.event.*;
 
 public class LionsLambs extends GraphicsProgram {
 	
+	public static Agent[] board = new Agent[25];
+
+	public static void init() {
+		board[0] = new Lion("Leo");
+		board[4] = new Lion("Leu");
+		board[20] = new Lion("Love");
+		board[24] = new Lion("Lejon");
+	}
+
 	public static void main() {
-		GCanvas canvas = new GCanvas();
-		GRect rect = new GRect(10, 10);
-		canvas.add(rect);
+		drawCanvas();
 	}
 }
