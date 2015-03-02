7577e055cb427d26d3f11f3bd54f94424c09ad2a
maartje <maartjeterhoeve@gmail.com>
2015-03-02 22:51:54 +0100

    BoardGraphics added

---
+++ assignment_3/BoardGraphics.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class BoardGraphics extends JFrame {
+
+    /* Constructor to set up the board frame */
+    public BoardGraphics {
+        JFrame board = new JFrame();
+        board.setSize(600, 600);
+        board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        board.setTitle("Lions and Lambs");
+        board.setLocation(50, 50);
+    }    
+
+}
