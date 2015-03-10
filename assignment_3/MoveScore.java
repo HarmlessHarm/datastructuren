b6fd32aa9fb83b946fed327d203a4a5a5d789eaa
maartje <maartjeterhoeve@gmail.com>
2015-03-10 16:01:12 +0100

    MoveScore class

---
+++ assignment_3/MoveScore.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class MoveScore {    
+      
+    int[] move;
+    int score; 
+       
+    public MoveScore() {
+        //constructor
+    }
+    
+    public MoveScore(int[] move, int score) {
+        // second constructor
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;   
+    }
+    
+    public void setMove(int[] move) {
+        this.move = move;
+    }
+    
+    public int[] getMove() {
+        move;
+    }
+
+}
